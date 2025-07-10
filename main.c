41a6320f878bc83053e158e6c160f5e970f14fca
Your Name <you@example.com>
2025-07-10 11:37:14 +0200

    rules added

--- main.c
+++ main.c
@@ -1,3 +1,16 @@
 /* basato sul video di Sanfilippo https://www.youtube.com/watch?v=c5atNuYdKK8 
 Conway's Game of Life https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life 
-*/
+
+Rules: 
+una cellula è sempre viva se ha sempre due o tre celle vicine vive
+viversa muore
+ _______
+|NW N NE|
+|W  C  E|
+|SW S SE|
+ _______
+
+una cellula morta diventa viva se ha esattamente tre celle vicine vive
+una cellula morta resta morta se ha meno di tre o più di tre celle vicine vive
+
+*/ 
